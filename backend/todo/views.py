afeaa691a60e1c92a1bf6b3669ed183c3d0c903d
Iasmini Gomes <iasmini.gomes@e-deploy.com.br>
2021-12-07 20:16:57 -0300

    outra forma de remover validacao csrf

--- backend/todo/views.py
+++ backend/todo/views.py
@@ -4,9 +4,9 @@ from .models import Todo
 
 
 class TodoView(viewsets.ModelViewSet):
-    # desabilita csrf para essas rotas
-    permission_classes = ()
-    authentication_classes = ()
+    # # desabilita csrf para essas rotas
+    # permission_classes = ()
+    # authentication_classes = ()
 
     serializer_class = TodoSerializer
     queryset = Todo.objects.all()
